60be8cf65ca785ce17a45f1559feeaca144c4f07
Yoonseo Han <yooncer00@gmail.com>
2024-05-12 01:34:29 +0800

    Finished data generation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from json import dumps
 
 # Kafka broker configuration
-kafka_nodes = "broker:9092"
+kafka_nodes = "kafka:9092"
 
 # Topic for NBA news
 nba_news_topic = 'nba-news'
@@ -18,16 +18,16 @@ teams = ["Boston celtics", "New York knicks"]
 boston_team_info = {
     "team_name": "Boston Celtics",
     "rank": 1,
-    "team_players": ["Jayson Tatum", "Jaylen Brown", "Derrick White", "Jrue Holiday", "Kristaps Portingis"]
+    "team_players": ["Jayson Tatum", "Jaylen Brown", "Derrick White", "Jrue Holiday", "Kristops Porzingis"]
 }
 boston_player_info = {
     "team_name": "Boston Celtics",
     "player_importance": {
-        "Jayson Tatum": 0.9,        
-        "Jaylen Brown": 0.85,       
-        "Derrick White": 0.7,        
-        "Jrue Holiday": 0.95,        
-        "Kristaps Portingis": 0.8  
+        "Jayson Tatum": 0.9,
+        "Jaylen Brown": 0.85,
+        "Derrick White": 0.7,
+        "Jrue Holiday": 0.95,
+        "Kristaps Portingis": 0.8
     }
 }
 
@@ -39,28 +39,57 @@ NY_team_info = {
 NY_player_info = {
     "team_name": "New York knicks",
     "player_importance": {
-        "Julius Randle": 0.9,         
-        "Josh Hart": 0.8,             
-        "Jalen Brunson": 0.85,        
-        "Donte Divincenzo": 0.75,     
-        "OG Anunoby": 0.85   
+        "Julius Randle": 0.9,
+        "Josh Hart": 0.8,
+        "Jalen Brunson": 0.85,
+        "Donte Divincenzo": 0.75,
+        "OG Anunoby": 0.85
     }
 }
 
-boston_players = ["Jayson Tatum", "Jaylen Brown", "Marcus Smart", "Al Horford"]
+boston_players = ["Jayson Tatum", "Jaylen Brown", "Derrick White", "Jrue Holiday", "Kristops Porzingis"]
 
-knicks_players = ["Julius Randle", "RJ Barrett", "Jalen Brunson", "donte divincenzo"]
+knicks_players = ["Julius Randle", "Josh Hart", "Jalen Brunson", "Donte Divincenzo", "OG Anunoby"]
 
-actions = ["committed a foul against", "blocked the shot of", "turned the ball over to"]
+actions = ["committed a foul against", "blocked the shot of", "turned the ball over to", "Scored 2 points against", "Scored 2 points against","Scored 2 points against", "Scored 3 points against", "Scored 3 points against"]
 
+# Function to send team and player information
+def send_initial_information(producer, topic, team_info, player_info):
+    # Send team information
+    producer.produce(topic, key=team_info['team_name'], value=json.dumps(team_info))
+    print(f"Sent team information for {team_info['team_name']}")
+
+    # Send player information
+    for player, importance in player_info['player_importance'].items():
+        player_data = {
+            "team_name": player_info['team_name'],
+            "player_name": player,
+            "importance": importance
+        }
+        producer.produce(topic, key=player, value=json.dumps(player_data))
+        print(f"Sent player information for {player}")
 
 # Generate random news
 def generate_nba_news():
-    player1 = random.choice(boston_players)
-    player2 = random.choice(knicks_players)
+    # Randomly choose between the two possibilities
+    option = random.randint(1, 2)
+
+    if option == 1:
+        player1 = random.choice(boston_players)
+        player2 = random.choice(knicks_players)
+    else:
+        player1 = random.choice(knicks_players)
+        player2 = random.choice(boston_players)
+
     action = random.choice(actions)
 
-    return f"{player1} {action} {player2}"
+    #return f"{player1} {action} {player2}"
+    return {
+        'player1': player1,
+        'player2': player2,
+        'action': action,
+        'updateTime': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f%z'),
+    }
 
 
 # Create Kafka producer
@@ -79,31 +108,35 @@ def delivery_report(err, msg):
         print(f'Message delivery failed: {err}')
     else:
         print(f"Message delivered to {msg.topic} [{msg.partition()}]")
-        
+
 
 def main():
     topic = 'game_updates'
     producer = SerializingProducer({
-        'bootstrap.servers': 'broker:29092'
+        'bootstrap.servers': 'localhost:9092'
     })
 
     curr_time = datetime.now()
 
+    # Initial setup
+    send_initial_information(producer, topic, boston_team_info, boston_player_info)
+    send_initial_information(producer, topic, NY_team_info, NY_player_info)
+
     # Only active for 120 seconds
     while (datetime.now() - curr_time).seconds < 120:
         try:
-            updates = publish_nba_news()
-            print("JASD")
+            updates = generate_nba_news()
+            print(updates)
 
             producer.produce(topic,
-                            key=updates['player'],
+                            key=updates['player1'],
                             value=json.dumps(updates),
                             on_delivery=delivery_report
                             )
             # To ensure data gets delivered before another one gets sent
             producer.poll(0)
             time.sleep(3)
-            
+
         except BufferError:
             print("Buffer full! Waiting...")
             time.sleep(1)

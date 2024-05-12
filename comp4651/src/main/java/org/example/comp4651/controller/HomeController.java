33b2fe29064ba1a5762ed20a00594dda3cc21812
sungjin122517 <sungjin122517@naver.com>
2024-05-12 19:31:37 +0800

    connect to postgresql in docker

--- comp4651/src/main/java/org/example/comp4651/controller/HomeController.java
+++ comp4651/src/main/java/org/example/comp4651/controller/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
 
     private final PlayerService playerService;
